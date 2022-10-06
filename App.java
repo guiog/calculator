9f63f610fe135cf6df54c52f9feb3bffe218de4e
gguio <gdguio@outlook.com>
2022-10-06 12:58:46 -0400

    My first version of the incomplete calculator code

---
+++ App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+    private static final Scanner inputScanner = new Scanner(System.in);
+
+    private static String getString(String prompt) {
+        System.out.println(prompt);
+    }
+
+    private static double getDouble(String prompt) {
+        System.out.println(prompt);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+    }
+}
