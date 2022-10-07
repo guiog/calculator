edf0650c8b18e6c53d04fea40404e01f916fb7d7
gguio <gdguio@outlook.com>
2022-10-07 15:44:02 -0400

    ignoring class files, adding calculator.java, and removing *Scan.java files

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+// Add substract multiply and divide methods
+
+public class Calculator {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
