7fae1bd3d6543cba2402766791dbf98762fd84a4
gguio <gdguio@outlook.com>
2022-10-08 16:45:31 -0400

    Major updates to calculator

--- App.java
+++ App.java
@@ -1,26 +1,49 @@
 import java.util.Scanner;
 
-// Inputs from terminal to specify calculators behavior
-
 public class App {
     static final Scanner inputScanner = new Scanner(System.in);
 
-    // private static String getString(String prompt) {
-    //     System.out.println(prompt);
-    //     return;
-    // }
+    private static String getString(String prompt) {
+        System.out.println(prompt);
+        String x = inputScanner.nextLine();
+        return x;
+    }
     
-    // private static double getDouble(String prompt) {
-    //     System.out.println(prompt);
-    //     return;
-    // }
-
-    
+    private static double getDouble(String prompt) {
+        System.out.println(prompt);
+        Double num = inputScanner.nextDouble();
+        return num;
+    }
 
     public static void main(String[] args) {
-        double num1 = 5;
-        double num2 = 3;
+        double num1;
+        double num2;
+        String operation = "";
         Calculator calc = new Calculator();
-        calc.add(num1, num2);
+        double calcAnswer;
+        
+        operation = getString("What would you like to do with the numbers? add, sub, mult, div");
+        num1 = getDouble("Please enter the first number ");
+        num2 = getDouble("Please enter the second number ");
+
+        if (operation == "add") {
+            System.out.println("I made it into add");
+            calcAnswer = calc.add(num1, num2);
+            System.out.println(calcAnswer);
+        }
+        if (operation == "sub") {
+            calcAnswer = calc.subtract(num1, num2);
+            System.out.println(calcAnswer);
+        }
+        if (operation == "mult") {
+            calcAnswer = calc.multiply(num1, num2);
+            System.out.println(calcAnswer);
+        }
+        if (operation == "div") {
+            calcAnswer = calc.divide(num1, num2);
+            System.out.println(calcAnswer);
+        }
+
+        System.out.println("Goodbye");
     }
 }
